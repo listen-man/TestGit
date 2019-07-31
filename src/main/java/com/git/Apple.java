eddfb091567b041ca5fa4b8e7453bbf8bbea7c5a
wangjian <wangjian@git.com>
2019-07-31 17:40:49 +0800

    it's my first commit in eclipse!

---
+++ src/main/java/com/git/Apple.java
@@ -0,0 +1,14 @@
+package com.git;
+/**
+ * 此类做测试Git用，无实际意义
+ * @author Admin
+ *
+ */
+public class Apple {
+
+	public Apple() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/com/test/git/Goods.java
@@ -0,0 +1,14 @@
+package com.test.git;
+/**
+ * 此类做测试Git用，无实际意义
+ * @author Admin
+ *
+ */
+public class Goods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
